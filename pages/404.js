75154c01c1b29f9c123bf3f36b68db79d1af63d4
tswaaaa <alt.tsovoo@gmail.com>
2022-12-10 12:41:56 +0800

    anhnii git upload

---
+++ pages/404.js
@@ -0,0 +1,6 @@
+export default function Custom404() {
+  return (
+    <h1>404 - Энэ хуудас олдсонгүй</h1>
+    <link>https://tenor.com/bKSc4.gif</link>
+  );
+}
---
+++ pages/introduction.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const introduction = () => {
+  return (
+    <div>introduction</div>
+  )
+}
+
+export default introduction
